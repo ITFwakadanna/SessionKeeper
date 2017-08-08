169a125dd4ec86e957566c1021fa8aa26811fea5
zenihirosi <zenihirosi@yahoo.co.jp>
2017-08-08 15:21:27 +0900

    first
    Copied files

---
+++ SessionKeeper/SessionKeeper.h
@@ -0,0 +1,16 @@
+//
+//  SessionKeeper.h
+//  SessionKeeper
+//
+//  Created by Yo on 2017/08/07.
+//  Copyright © 2017年 Yo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "FlashRuntimeExtensions.h"
+
+
+@interface SessionKeeper : NSObject
+
+@end
